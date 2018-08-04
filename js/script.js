c4dfcbd97b1966c217fe4712847787b6c11488d4
Peter Retvari <pretvari@gmail.com>
2018-08-04 15:45:06 +0200

    Create functions
    Create function to show the page number regarding the students volume and another function to hide the students on the page

--- js/script.js
+++ js/script.js
@@ -14,54 +14,41 @@ const AllStudent = document.querySelectorAll('ul li');
 let PageToDisplay;
 
 
-// Create a function to hide all of the items in the list excpet for the ten you want to show
 
-// The pagenumber is the fundamental of the pagination. How to calculate the proper number of page we will need?
-// It comes from x(length of the ul array) devided by 10.
-// First check x. If it's smaller than 10 then the pagenumber is 1.
-//If the number is a whole number then it's equal to the pagenumber
-//If the number is not a whole number then pagenumber = x/10+1
+//FUNCTIONS
 
-  const getPageNumber = () => {
+ //Show me the proper page number regarding the students
 
-    if (studentLength < 10) {
+ const getPageNumber = () => {
 
-      pNumberDisplay = studentLength;
+   if (StudentLength <10) {
 
-    } else if (Number.isInteger(studentLength/10)) {
+       PageToDisplay = 1;
 
-      pNumberDisplay = studentLength/10;
-    } else {
-
-      pNumberDisplay = Math.ceil(studentLength/10) ;
-
-    }
-
-    return pNumberDisplay;
-  }
-
-  const showPage = (pagenumber,studentlist) => {
-
-    // first hide all students on the page
-
-document.querySelector('.student-list').style.visibility = 'hidden';
+   } else if (Number.isInteger(StudentLength / 10))  {
 
 
+       PageToDisplay = StudentLength/10;
 
-// then loop through all students in our student list argument
-// if student should be on this page number
-// show the student
+   } else {
 
-// Tip: Keep in mind that with a list of 54 studetns, the last page will only display four
+       PageToDisplay = Math.ceil((StudentLength/10));
+   }
 
+
+   return PageToDisplay;
+
+ }
+
+ // Hide all students
+
+const hideAllStudent = () => {
+
+
+ for (let i = 0; i < StudentLength; i +=1) {
+
+     // select the i student from AllStudent
+     AllStudent[i].style.display= "none";
+
+   }
 }
-
-
-
-// Create and append the pagination links - Creating a function that can do this is a good approach
-
-
-
-
-// Add functionality to the pagination buttons so that they show and hide the correct items
-// Tip: If you created a function above to show/hide list items, it could be helpful here
